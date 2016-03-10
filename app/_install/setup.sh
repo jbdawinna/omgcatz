bbc8c0b2fbf75ac0c05eff1c1cbaf188cd69879b
jbdawinna <jbrivers@roadrunner.com>
2016-03-10 14:50:49 -0500

    Update setup.sh

--- app/_install/setup.sh
+++ app/_install/setup.sh
@@ -24,42 +24,14 @@ SQL_SERVER=${SQL_SERVER:-localhost}
 echo -en "MySQL db name [omgcatz]: "; read DB_NAME
 DB_NAME=${DB_NAME:-omgcatz}
 
-echo -en "\nAre you going to use minion servers? [y/n]: "; read HAS_MINIONS
-if [ "$HAS_MINIONS" == "y" ]; then
-	while :; do
-		echo -en "minion root (e.g. http://your_server.com/): "; read SERVER
-		[ -z "$SERVER" ] && break
-		SERVERS="$SERVERS\"$SERVER\","
-	done
 
-	MINIONS="public static \$minions = array($SERVERS);"
-else
-	if [ ! -d "../api/stuff/download" ]; then
-		echo
-		git clone "https://github.com/cadejscroggins/omgcatz-minion/" "../api/stuff/download"
-		echo -e "\nRunning api/stuff/download/_install/setup.sh..."
-		../api/stuff/download/_install/setup.sh
-	fi
+if [ ! -d "../api/stuff/download" ]; then
+	echo
+	git clone "https://github.com/cadejscroggins/omgcatz-minion/" "../api/stuff/download"
+	echo -e "\nRunning api/stuff/download/_install/setup.sh..."
+	../api/stuff/download/_install/setup.sh
 fi
 
 # create Config class
 echo "<?php class Config { public static \$server=\"$SQL_SERVER\",\$user=\"$NAME\",\$password=\"$PASS\",\$database=\"$DB_NAME\";$MINIONS }" > ../api/include/Config.php
 
-# optionally create database
-echo -en "\nCreate database $DB_NAME? [y/n]: "; read ANSWER
-if [ "$ANSWER" == "y" ]; then
-	echo -en "Creating database..."
-	echo "create database $DB_NAME" | mysql -u"$NAME" -h"$SQL_SERVER" -p"$PASS" &> /dev/null && success || failure
-fi
-
-# optionally create tables
-echo -en "\nCreate tables in $DB_NAME? [y/n]: "; read ANSWER
-if [ "$ANSWER" == "y" ]; then
-	echo -en "Creating tables..."
-	php ./create-tables.php && success || failure
-
-	if [ "$HAS_MINIONS" == "y" ]; then
-		echo -en "Adding minion servers..."
-		php ./install-minions.php && success || failure
-	fi
-fi
